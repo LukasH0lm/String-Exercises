9ac5d2bcb4f914cab41cf8ca9dcd203906fa6090
lukash0lm <lukasholm@hotmail.com>
2022-10-02 17:58:47 +0200

    updated files

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         int masterExercise = 11;
-        String masterInput = "Hello*2";
-        String masterFilter = "ng";
+        String masterInput = "";
+        String masterFilter = "";
         switch(masterExercise){
             case 1 -> System.out.println(exercise1(masterInput));
             case 2 -> System.out.println(exercise2(masterInput));
